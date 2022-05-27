41d4790516d94a9def7ff55f37ca21d581bde21f
myusername <myemail>
2022-05-28 01:29:14 +0530

    service section added

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -9,6 +9,16 @@ function Service() {
                         <p>What I do</p>
                         <h1>I've got everything you need to launch your website.</h1>
                     </div>
+                    <div className="service__parent__box">
+                        <div className="service__box">
+                            <p>Web Development</p>
+                            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. A vero quas accusamus rerum blanditiis corporis culpa dignissimos ipsum ratione asperiores.</h1>
+                        </div>
+                        <div className="service__box">
+                            <p>Web Design</p>
+                            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. A vero quas accusamus rerum blanditiis corporis culpa dignissimos ipsum ratione asperiores.</h1>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
