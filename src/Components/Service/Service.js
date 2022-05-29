339d298b734991b5b02705b9c156f6289165bcf1
myusername <myemail>
2022-05-30 01:39:45 +0530

    scroll fade animation added to service section

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -1,22 +1,30 @@
 import "./Service.css"
 
+import { useEffect } from "react"
+
+import Aos from 'aos'
+import "aos/dist/aos.css"
 function Service() {
+
+    useEffect(() => {
+        Aos.init({ duration: 1000 })
+    })
     return (
         <>
             <div className="parent__service">
                 <div id="service">
-                    <div className="service__heading">
+                    <div className="service__heading" data-aos="fade-up">
                         <p>What I do</p>
                         <h1>I've got everything you need to launch your website.</h1>
                     </div>
                     <div className="service__parent__box">
-                        <div className="service__box">
+                        <div className="service__box" data-aos="fade-up">
                             <p>Web Development</p>
-                            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. A vero quas accusamus rerum blanditiis corporis culpa dignissimos ipsum ratione asperiores.</h1>
+                            <h1>Development of static and dynamic websites of your like. With highly optimized and deployed on a perfect domain. And bla bla bla Bla  </h1>
                         </div>
-                        <div className="service__box">
+                        <div className="service__box" data-aos="fade-up">
                             <p>Web Design</p>
-                            <h1>Designing of interactive and creative webpages for the website in industrial leading software. With great skills comes great quality. </h1>
+                            <h1>Designing of interactive and creative webpages for the website in industrial leading software. Provided with high quality files and three times editable.</h1>
                         </div>
                     </div>
                 </div>
