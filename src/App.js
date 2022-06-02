4f82aad6ba06d16ae03254bf5daa87c755f87c67
myusername <myemail>
2022-06-02 19:28:49 +0530

    works section disable for some moment

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <Home />
       <About />
       <Service />
-      <Work />
+      {/* <Work /> */}
     </>
   );
 }
--- src/Components/redux/store.js
+++ src/Components/redux/store.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk'
 import rootReducer from './reducers/rootReducer'
 
 const middlewares = [thunk]
+
 if (process.env.NODE_ENV === "development") {
 
     const { logger } = require('redux-logger');
