5bc962d753597315b0e62d3bf2245b0436286934
myusername <myemail>
2022-06-23 22:55:51 +0530

    footer section for mobiler is added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,19 +3,30 @@
 import React from 'react'
 import './Footer.css'
 
+import { AiFillInstagram, AiFillGithub, AiFillLinkedin, AiFillBehanceSquare } from 'react-icons/ai'
+
 function Footer() {
     return (
         <>
             <footer>
                 <div className="footer__main__parent">
                     <div className="footer__top">
-                        <h1>Portfolio.</h1>
-                        <p>
-                            It is my pleasure to welcome you to my portfolio.
-                            It is my pleasure to welcome you to my portfolio.
-                            It is my pleasure to welcome you to my portfolio.
-                            It is my pleasure to welcome you to my portfolio.
-                        </p>
+                        <div className="footer__left">
+                            <h1>Portfolio.</h1>
+                            <p>
+                                It is my pleasure to welcome you to my portfolio.
+                                It is my pleasure to welcome you to my portfolio.
+                                It is my pleasure to welcome you to my portfolio.
+                                It is my pleasure to welcome you to my portfolio.
+                            </p>
+                        </div>
+
+                        <div className="footer__right">
+                            <a href="https://www.instagram.com/kant69__/" target="_blank" ><AiFillInstagram /></a>
+                            <a href="https://github.com/DeepanshuKant" target="_blank"><AiFillGithub /></a>
+                            <a href="https://www.linkedin.com/in/deepanshu-kant-673b2a1b4/" target="_blank"><AiFillLinkedin /></a>
+                            <a href="https://www.behance.net/deepanshukant" target="_blank"><AiFillBehanceSquare /></a>
+                        </div>
                     </div>
 
                     <div className="footer__bottom">
