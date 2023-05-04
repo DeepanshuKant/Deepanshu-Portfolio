a6a7877c35adcdca116fef30201ea6ad77454fe7
myusername <myemail>
2023-05-04 07:52:36 +0530

    removed alert

--- src/App.js
+++ src/App.js
@@ -9,22 +9,8 @@ import Work from './Components/work/Work';
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer';
 
-import { useDispatch } from 'react-redux'
-import { useEffect } from 'react'
-import { getAllWorks } from './Components/redux/reducers/works/works.action'
 
 function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-
-    alert("All the data about awards and projects is dummy. If you want to contact me, please use the contact form. Thank you!")
-
-    function getData() {
-      dispatch(getAllWorks());
-    }
-    getData();
-  }, [])
-
   return (
     <>
       <Header />
