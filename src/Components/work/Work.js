355f3c0aa0a2ebc8858b8ef1ba0e5f8a2072f028
myusername <myemail>
2023-06-29 00:53:53 +0530

    works section redesign

--- src/Components/work/Work.js
+++ src/Components/work/Work.js
@@ -1,7 +1,7 @@
 import './Work.css'
 import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-
+import { BiLinkExternal } from 'react-icons/bi'
 import { motion } from 'framer-motion'
 
 import Aos from 'aos'
@@ -69,8 +69,22 @@ function Work() {
                         <h1>I love what I do,<br /> check out some my of latest works </h1>
                     </div>
 
+
+                    <div className="work__items">
+                        <ul className="work__items__ul">
+                            {workData && workData.map((work) => (
+                                <li className="work__list">
+                                    {work.name}
+                                    <a href={work.url} target='_blank' >
+                                        <span> <BiLinkExternal size={25} /></span>
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+
                     {/* <div className="work__parent__box" > */}
-                    <div className="work__parent__box" data-aos="fade-up" >
+                    {/* <div className="work__parent__box" data-aos="fade-up" >
                         {workData.map((works) =>
                             <div className="work__box" key={works._id}
                                 style={{ background: `url(${works.image})`, backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} >
@@ -78,7 +92,7 @@ function Work() {
                                 <a href={works.url} target="_blank" >Visit</a>
                             </div>
                         )}
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </>
